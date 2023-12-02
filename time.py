ea6966c5b8c2c9690ca4597299c08012c002e902
Setayesh Gholami <137152042+Setayesh1375@users.noreply.github.com>
2023-12-02 06:48:15 +0330

    Add files via upload

---
+++ time.py
@@ -0,0 +1,11 @@
+class time:
+    def __init__(self,s,m,h):
+        self.seconds = s
+        self.minute = m
+        self.hours = h
+
+    def passing(self):
+        ...
+    
+    def happen(self):
+        ...
